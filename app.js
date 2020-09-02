93e3ef1b867d2d94b6c6f1dc7d6739f9b5526140
Ryan Unruh <ryanunruhmedia@gmail.com>
2020-09-02 10:19:41 -0500

    Final

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ document.querySelector(".get-jokes").addEventListener("click", getJokes);
 
 function getJokes(e) {
   const number = document.querySelector('input[type="number"]').value;
-
   const xhr = new XMLHttpRequest();
-
   xhr.open("GET", `http://api.icndb.com/jokes/random/${number}`, true);
 
   xhr.onload = function () {
@@ -18,12 +16,37 @@ function getJokes(e) {
       } else {
         output += "<li>Something went wrong</li>";
       }
-      console.log(response);
-      document.querySelector(".jokes").innerHTML = output;
+      document.getElementById("jokes").innerHTML = output;
     }
   };
-
   xhr.send();
-
   e.preventDefault();
 }
+
+// function getJokes(e) {
+//   const number = document.querySelector('input[type="number"]').value;
+
+//   const xhr = new XMLHttpRequest();
+
+//   xhr.open("GET", `http://api.icndb.com/jokes/random/${number}`, true);
+
+//   xhr.onload = function () {
+//     if (this.status === 200) {
+//       const response = JSON.parse(this.responseText);
+//       let output = "";
+//       if (response.type === "success") {
+//         response.value.forEach((joke) => {
+//           output += `<li>${joke.joke}</li>`;
+//         });
+//       } else {
+//         output += "<li>Something went wrong</li>";
+//       }
+//       console.log(response);
+//       document.getElementById("jokes").innerHTML = output;
+//     }
+//   };
+
+//   xhr.send();
+
+//   e.preventDefault();
+// }
